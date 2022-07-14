dcff950d0eb30237c74c108accf7b2ce63bdb171
zerry101 <macone394@gmail.com>
2022-07-14 14:15:43 +0530

    n

--- dailytask2/src/search/search.component.ts
+++ dailytask2/src/search/search.component.ts
@@ -6,9 +6,16 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-
+color:string="";
+id:number=0;
   constructor(private ar:ActivatedRoute) {
-    this.ar.queryParams.subscribe((data)=>{console.log(data)})
+    this.ar.queryParams.subscribe((data)=>{
+      this.color=data['color'];
+      this.id=data['id'];
+      console.log(this.color+" "+this.id);
+      // console.log(this.color);
+    })
+
    }
 
   ngOnInit(): void {
